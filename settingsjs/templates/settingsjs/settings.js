c2d7f2381c0009b3f657d898a9b8fba493821dd2
Alex K <alex@kamedov.ru>
2018-01-04 10:59:21 +0500

    add static to API, use ES6 in settings file

--- settingsjs/templates/settingsjs/settings.js
+++ settingsjs/templates/settingsjs/settings.js
@@ -1,10 +1,22 @@
-'use strict';
 
-const settings = {{ jssettings|safe }};
+const settings = {{ jssettings|safe }}
 
 const S = {
-	get: function (key) { return settings[key]; },
-	set: function (key, val) { return settings[key] = val; }
+    get: function(key, default_) {
+        if (settings[key] === undefined)
+            return default_
+        else
+            return settings[key]
+    },
+    set: function(key, val) {
+        settings[key] = val
+    },
+    static: function(key) {
+        if (key)
+            return this.get('STATIC_URL') + key
+        else
+            return this.get('STATIC_URL')
+    }
 };
 
-module.exports = S;
+module.exports = S
